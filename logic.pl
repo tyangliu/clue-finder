4208c731b547b29b37c443bd24c48bbddde3e65f
tyangliu <tyangliu@gmail.com>
2014-11-26 23:21:03 -0800

    format logic.pl

--- logic.pl
+++ logic.pl
@@ -1,27 +1,39 @@
 
-Holds(P,CardC):-
-HoldsOneOf(P,CardA,CardB,CardC),cantHold(P, CardB),cantHold(P,cardA)
+Holds(P,CardC) :-
+	HoldsOneOf(P,CardA,CardB,CardC), 
+	cantHold(P,CardB), 
+	cantHold(P,CardA).
 
-Holds(P,CardB):-
-HoldsOneOf(P,CardA,CardB,CardC),cantHold(P, CardA),cantHold(P,CardC)
+Holds(P,CardB) :-
+	HoldsOneOf(P,CardA,CardB,CardC),
+	cantHold(P, CardA),
+	cantHold(P,CardC).
 
-Holds(P,CardA):-
-HoldsOneOf(P,CardA,CardB,CardC),cantHold(P, CardB),cantHold(P,CardC
+Holds(P,CardA) :-
+	HoldsOneOf(P,CardA,CardB,CardC),
+	cantHold(P, CardB),
+	cantHold(P, CardC).
 
-Holds(P,CardA):-
-HoldsTwoOf(P,cardA,CardB,CardC),cantHold(P,CardC)
+Holds(P,CardA) :-
+	HoldsTwoOf(P,CardA,CardB,CardC),
+	cantHold(P,CardC).
 
-Holds(P,CardB):-
-HoldsTwoOf(P,cardA,CardB,CardC),cantHold(P,CardC)
+Holds(P,CardB) :-
+	HoldsTwoOf(P,CardA,CardB,CardC),
+	cantHold(P,CardC).
 
-Holds(P,CardA):-
-HoldsTwoOf(P,cardA,CardB,CardC),cantHold(P,CardB)
+Holds(P,CardA) :-
+	HoldsTwoOf(P,CardA,CardB,CardC),
+	cantHold(P,CardB).
 
-Holds(P,CardC):-
-HoldsTwoOf(P,cardA,CardB,CardC),cantHold(P,CardB)
+Holds(P,CardC) :-
+	HoldsTwoOf(P,CardA,CardB,CardC),
+	cantHold(P,CardB).
 
-Holds(P,CardB):-
-HoldsTwoOf(P,cardA,CardB,CardC),cantHold(P,CardA)
+Holds(P,CardB) :-
+	HoldsTwoOf(P,CardA,CardB,CardC),
+	cantHold(P,CardA).
 
-Holds(P,CardC):-
-HoldsTwoOf(P,cardA,CardB,CardC),cantHold(P,CardA)
+Holds(P,CardC) :-
+	HoldsTwoOf(P,CardA,CardB,CardC),
+	cantHold(P,CardA)
